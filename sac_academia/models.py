cc314c00a166038bea2c078367cb739d8e2106ae
Frank Gabriel <fgabrieloliveira1@gmail.com>
2025-12-29 23:29:42 -0300

    Correção no painel de mensagens

--- sac_academia/models.py
+++ sac_academia/models.py
@@ -2,15 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
     
-
-
-
 class Mensagem(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     nome = models.CharField(max_length=120)
     email = models.EmailField()
     mensagem = models.TextField()
     data_envio = models.DateTimeField(auto_now_add=True)
+    lida = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.nome} <{self.email}>"
